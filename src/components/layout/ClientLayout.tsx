a2e8ada398212a8bd9722b6aceb2466fb13814a1
y99AICredit Developer <db@y99.vn>
2026-03-01 10:39:50 +0700

    refactor: remove skip to main content link
    - Delete SkipLink component
    - Remove from ClientLayout

--- src/components/layout/ClientLayout.tsx
+++ src/components/layout/ClientLayout.tsx
@@ -5,12 +5,10 @@ import ScrollToTopButton from '@/components/layout/ScrollToTopButton';
 import QuickCallButton from '@/components/layout/QuickCallButton';
 import ScrollRestoration from '@/components/layout/ScrollRestoration';
 import SmoothScroll from '@/components/layout/SmoothScroll';
-import { SkipLink } from '@/components/layout/SkipLink';
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <SkipLink />
       <SmoothScroll />
       <ScrollRestoration />
       {children}
